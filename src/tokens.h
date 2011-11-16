1612f5a569299e5299058e224e535cc36bf2c12d
Cassiano Rocha Kuplich <cassianorocha@gmail.com>
2011-11-15 22:09:59 -0200

    Etapa 1

---
+++ src/tokens.h
@@ -0,0 +1,34 @@
+/**
+ * Lista dos tokens, com valores constantes associados.
+ * Este arquivo tera vocacao a ser incluido em varios outros.
+ * A escolha dos valores constantes eh totalmente arbitraria.
+ * Cada valor deve ser distinto.
+ */
+#define INT 100
+#define DOUBLE 101
+#define REAL 102
+#define CHAR 103
+#define QUOTE 104
+#define STRING 105
+#define LE 106
+#define GE 107
+#define EQ 108
+#define NE 109
+#define AND 110
+#define OR 111
+#define NOT 112
+#define IF 113
+#define THEN 114
+#define ELSE 115
+#define WHILE 116
+#define FOR 117
+#define NEXT 118
+#define END 119
+#define TRUE 120
+#define FALSE 121
+
+#define IDF 200
+#define INT_LIT 201
+#define F_LIT 202
+#define CONST 203
+#define STR_LIT 204
